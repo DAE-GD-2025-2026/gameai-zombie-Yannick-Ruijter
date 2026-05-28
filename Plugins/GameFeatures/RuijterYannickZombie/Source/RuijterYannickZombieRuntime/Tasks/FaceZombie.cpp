18c652f2d7545e1b1cacf6f688fdc66713d7bb6a
yannick ruijter <ruyter6969@gmail.com>
2026-05-28 21:44:17 +0200

    added task for looking at the zombie

---
+++ Plugins/GameFeatures/RuijterYannickZombie/Source/RuijterYannickZombieRuntime/Tasks/FaceZombie.cpp
@@ -0,0 +1,23 @@
+#include "FaceZombie.h"
+#include "AIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+UFaceZombie::UFaceZombie()
+{
+	bNotifyTick = true;
+	NodeName = TEXT("Aim at zombie");
+}
+
+EBTNodeResult::Type UFaceZombie::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	auto AIController = OwnerComp.GetAIOwner();
+	auto BlackBoard = AIController->GetBlackboardComponent();
+	auto TargetZombie = BlackBoard->GetValueAsObject(ZombieKeyName);
+	AIController->SetFocus(Cast<AActor>(TargetZombie));
+	return EBTNodeResult::Succeeded;
+}
+
+FString UFaceZombie::GetStaticDescription() const
+{
+	return FString::Printf(TEXT("Aims in the direction of the zombie"));
+}
